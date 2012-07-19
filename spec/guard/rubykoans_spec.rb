1e258002a679af2b8c97a1e51c5b672a87aeac40
lewy <lewy313@gmail.com>
2012-07-19 22:05:08 +0200

    changed spec

--- spec/guard/rubykoans_spec.rb
+++ spec/guard/rubykoans_spec.rb
@@ -4,6 +4,6 @@ require 'spec_helper'
 describe Guard::Rubykoans do
   it 'should work ok' do
     subject.should_receive(:system).with("ruby path_to_enlightenment.rb").and_return(true)
-    subject.run_on_change([])
+    subject.run_on_changes([])
   end
 end
